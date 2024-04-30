41c3f2b35e755afa4dfadefabd849bb3bdbc1516
Kirill <taranyuk31@mail.ru>
2024-04-30 16:57:27 +0400

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  if (d < 0) {
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
+
+
   return arr;
 }
 
