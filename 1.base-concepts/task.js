84c17f8bcea2b807d1fac3e726b17ccccd53e295
Kirill <taranyuk31@mail.ru>
2024-05-06 13:36:25 +0400

    cheange commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,11 +1,5 @@
 "use strict"
 
- @param {number} a;
- @param {number} b;
- @param {number} c;
- @returns {number[]};
-
-
 function solveEquation(a, b, c) {
   const d = b ** 2 - 4 * a * c;
 
@@ -14,20 +8,24 @@ function solveEquation(a, b, c) {
   } else if (d > 0) {
     const sqrtD = Math.sqrt(d);
     return [
-      (-b + sqrtD) / (2 * a),
-      (-b - sqrtD) / (2 * a),
+      (-b + Math.sqrt(d) )/(2*a),
+      (-b - Math.sqrt(d) )/(2*a),
     ];
   } else {
     return [];
 }
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  const monthlyInterestRate = percent / 100 / 12;
-  const bodyOfLoan = amount - contribution;
-  const monthlyPayment = bodyOfLoan * (monthlyPercent +
-    (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1))) * countMonths;
-  const roundedMonthlyPayment = parseFloat(monthlyPayment.toFixed(2));
-
-  return +parseFloat(totalMortgagePayment).toFixed(2);
 }
 
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  const interestRate = percent / 100 / 12;
+  const bodyOfCredit = amount - contribution;
+
+  if (bodyOfCredit === 0) {
+    return 0;
+  }
+
+  const monthlyPayment = bodyOfCredit * (interestRate + (interestRate / (((1 + interestRate) ** countMonths) - 1)));
+  const totalPayment = monthlyPayment * countMonths;
+
+  return +totalPayment.toFixed(2);
+}
