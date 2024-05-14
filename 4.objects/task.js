80b4709d7d14029136eeab75766df905cc0aed9b
Kirill <taranyuk31@mail.ru>
2024-05-14 14:18:00 +0400

    Cheange commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,66 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    this.excluded = false;
+  }
   
-}
+  Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+  };
+  
+  Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.excluded) {
+      console.log('Не удается выставить оценки исключенному ученику.');
+      return;
+    }
+  
+    if (!this.marks) {
+      console.log('Не удается добавить метки в несуществующий массив меток.');
+      return;
+    }
+  
+    this.marks.push(...marksToAdd);
+  };
+  
+  Student.prototype.getAverage = function() {
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
+  
+    const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks.length;
+  };
+  
+  Student.prototype.exclude = function(reason) {
+    if (this.excluded) {
+      console.log('Студент уже исключен.');
+      return;
+    }
+  
+    this.excluded = true;
+    delete this.marks;
+    delete this.subject;
+    this.exclusionReason = reason;
+  };
+  
+ 
+  const student1 = new Student('Dima', 'Male', 18);
+  const student2 = new Student('Diana', 'Female', 19);
+  
 
-Student.prototype.setSubject = function (subjectName) {
+  student1.setSubject('Математика');
+  student2.setSubject('Английский');
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  student1.addMarks(85, 90, 80);
+  student2.addMarks(95, 92, 88);
   
-}
-
-Student.prototype.getAverage = function () {
+  console.log(student1.getAverage());
+  console.log(student2.getAverage()); 
+ 
+  student1.exclude('Low attendance');
   
-}
-
-Student.prototype.exclude = function (reason) {
+  student1.addMarks(70, 80); 
   
-}
+  console.log(student1.getAverage());
